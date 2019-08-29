5ec3ed7088bde6f18525dfb18dd2175258a596eb
Gary Chen <garychen.whc@gmail.com>
2019-08-29 14:02:11 +0000

    cat breedFetcher done

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+const request = require("request");
+const userInput = process.argv.slice(2).join("");
+
+request("https://api.thecatapi.com/v1/breeds/", (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+
+    for (let breed of data) {
+      if (breed.name === userInput) {
+        console.log(breed.description);
+        break;
+      }
+      
+      if (breed.name === data[data.length - 1].name) {
+        console.log("Breed name is not found.");
+      }
+    }
+  }
+});
